4edee81c49792245d1e1fc7aaceb4c1dc8509948
Олександр Чорновол <oleksandrchornovol@MacBook-Pro-Oleksandr.local>
2021-01-30 17:06:07 +0200

    crud

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="chuck" src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png"/>
+  <div class="joke">{{ joke.value }}</div>
+  <button class="refresh"
+          @click="getRandomJoke"
+  >Refresh</button>
+  <div v-for="comment in joke.comments"
+       :key="comment.id"
+       class="comments"
+  >
+    <span class="comment">{{comment.text}}</span>
+    <button class="rm-comment"
+            @click="removeComment(comment.id)"
+    >Remove</button>
+  </div>
+  <div class="add-comment">
+    <input placeholder="Comment..."
+           v-model="newComment"/>
+    <button @click="addComment">Send</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      joke: {},
+      newComment: ''
+    }
+  },
+  mounted () {
+    this.getRandomJoke()
+  },
+  methods: {
+    addComment () {
+      this.axios.post('jokeComment', {
+        'joke_id': this.joke.id,
+        'text': this.newComment
+      }).then((response) => {
+        this.joke.comments.push(response.data)
+        this.newComment = ''
+      })
+    },
+    getRandomJoke () {
+      this.axios.get('getRandomJoke').then((response) => this.joke = response.data)
+    },
+    removeComment (commentId) {
+      this.axios.delete(`/jokeComment/${commentId}`).then(() => {
+        this.joke.comments = this.joke.comments.filter(c => c.id !== commentId)
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+img {
+  width: 300px;
+  margin: 25px auto;
+  display: block;
+}
+
+.joke {
+  margin: 40px auto 5px;
+  padding: 10px;
+  width: 300px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border: 1px solid black;
+  border-radius: 25px;
+}
+
+.refresh {
+  margin: 0 auto 15px;
+  text-align: center;
+  display: block;
+}
+
+input {
+  width: 300px;
+}
+
+.add-comment {
+  width: 300px;
+  margin: 10px auto;
+  right: 0;
+  left: 0;
+  display: flex;
+}
+
+.comments {
+  margin: 10px auto;
+  width: 300px;
+  right: 0;
+  left: 0;
+  background-color: lightgrey;
+  padding-left: 12px;
+  border-radius: 10px;
+  display: flex;
+}
+
+.comment {
+  width: 350px;
+  padding: 3px;
+}
+
+.rm-comment {
+  height: 25px;
+  padding: 3px;
+  border-radius: 0 10px 10px 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+axios.defaults.baseURL = 'http://127.0.0.1/api/'
+app.use(VueAxios, axios)
+app.mount('#app')
